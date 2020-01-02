98d6104c32aaa4952f8f65a9192b83b7167236a9
vuthanhtung <vuthanhtunghayquen98@gmail.com>
2020-01-02 10:58:37 +0700

    fix package.sql error

--- package.sql
+++ package.sql
@@ -22,7 +22,7 @@ CREATE OR REPLACE PACKAGE BODY GET_INFO AS
     return titleOut;
   END get_book_title;
     -----------------------------------------------------------
-  function get_book_author(empId number) return department.name%TYPE AS
+  function get_book_author(bookId number) return book.author%TYPE AS
     titleOut book.title%TYPE;
     authorOut book.author%TYPE;
   BEGIN
